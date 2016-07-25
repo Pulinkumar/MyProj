cc7715ce3dc4de9f64db465818baad24ea385ff3
PAkbari <PAkbari@335DPC829>
2016-07-25 09:17:24 -0400

    Add project files.

---
+++ Splendent.MyProject.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Splendent.MyProject.Web.UI.Startup))]
+namespace Splendent.MyProject.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
